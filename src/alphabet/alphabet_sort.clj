37770d44adc9c1ff0bfd45295efb3dff51a38eb3
Charlotte Fereday <cfereday@thoughtworks.com>
2018-04-08 21:21:02 +0100

    [Charlotte] Refactors alphabet check to use destructuring

--- src/alphabet/alphabet_sort.clj
+++ src/alphabet/alphabet_sort.clj
@@ -1,19 +1,20 @@
 (ns alphabet.alphabet_sort)
 
 (def alphabet
-  (set [:d :f :e :j :a :i :b :g :h :c]))
+  (set ["d" "f" "e" "j" "a" "i" "b" "g" "h" "c"]))
 
 (defn check-letter [letter]
   (contains? alphabet letter))
 
-(defn translate [word]
-  (into #{} (map keyword word)))
-
 (defn in-alphabet? [word]
-  (let [translated-word (translate word)
-        first-from-set (first translated-word)
-        popped-set (pop (into [] translated-word))
-        is-letter-in-alphabet (check-letter first-from-set)]
-    (if (true? ((some? popped-set) is-letter-in-alphabet))
-      (in-alphabet? popped-set))))
-
+  (let [[first & rest] word]
+    (prn "HERE IS FIRST" first)
+    (prn "HERE IS REST" rest)
+    (if (= (count rest) 0)
+      (do
+        (if (= (check-letter first) false)
+          false))
+      (do
+        (if (= (check-letter first) true)
+          (in-alphabet? rest)
+          true)))))
