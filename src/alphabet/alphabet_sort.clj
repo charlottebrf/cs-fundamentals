2c9b321a628e4fb110a08190deaa452eabbdeb26
Charlotte Fereday <cfereday@thoughtworks.com>
2018-04-08 14:49:07 +0100

    [Charlotte] Program translates the word input from a vector of strings to a set of keywords

--- src/alphabet/alphabet_sort.clj
+++ src/alphabet/alphabet_sort.clj
@@ -3,5 +3,16 @@
 (def alphabet
   (set [:d :f :e :j :a :i :b :g :h :c]))
 
+(defn check-letter [letter]
+  (contains? alphabet letter))
+
+(defn translate [word]
+  (if (nil? word)
+    #{}
+    (into #{} (map keyword word))))
+
 (defn in-alphabet? [word]
   false)
+
+
+
--- test/alphabet/alphabet_test.clj
+++ test/alphabet/alphabet_test.clj
@@ -7,10 +7,26 @@
     (is (= (count alphabet) 10))
     (is (= (type alphabet) clojure.lang.PersistentHashSet))))
 
+(deftest translate-word
+  (testing "if a word is nil it will be translated as an empty set"
+    (is (= (translate nil) #{}))
+    (is (= (translate [nil]) #{nil})))
+;:TODO how would I turn a string into a vector and then into a map of keywords?
+  (testing "a single word is translated as a keyword in a set"
+    (is (= (translate ["w"]) #{:w})))
+  (testing "words coming into the program as a vector of strings are translated to a set of keywords"
+    (is (= (translate ["w" "o" "r" "d"]) #{:w :o :r :d})))
+  (testing "duplicate letters in translated sets will be removed"
+    (is (= (translate ["w" "o" "r" "d" "w" "o"]) #{:w :o :r :d}))))
+
 (deftest check-word
-  (testing "an empty word does not appear in the alphabet"
+  (testing "an empty word is not found in the alphabet"
     (is (= (in-alphabet? nil) false)))
-  (testing "letters outside the alphabet are not found in the alphabet"
-    (is (= (in-alphabet? :y) false)))
-  (testing "multiple letters outside the alphabet are not found in the alphabet"
-    (is (= (in-alphabet? [:x :y :z])))))
+  (testing "a letter outside the alphabet are not found"
+    (is (= (in-alphabet? "y") false)))
+  (testing "multiple letters outside the alphabet are not found"
+    (is (= (in-alphabet? ["x" "y" "z"]))))
+  (testing "a letter in the alphabet is found"
+    (is (= (in-alphabet? "d") true)))
+  (testing "multiple letters from the alphabet are found"
+    (is (= (in-alphabet? ["a" "d"]) true))))
